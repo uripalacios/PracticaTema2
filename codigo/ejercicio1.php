5792e57ee0cfa4ae80fdc23d1f15e1ef7eeed9ab
uriel <uriel.palalo@educa.jcyl.es>
2021-10-07 17:54:50 +0000

    segundo commit practica temma2

--- codigo/ejercicio1.php
+++ codigo/ejercicio1.php
@@ -1,10 +1,16 @@
 <?php
 //mostrar fichero
+//buscar $_SERVER en el manual
 $nombrepagina = $_GET;
 echo $nombrepagina;
 echo basename($_SERVER);
 
 //mostrar la IP del equipo
+
+
+
+// calcula la fecha
+
 
 echo "<br>";
 ?>

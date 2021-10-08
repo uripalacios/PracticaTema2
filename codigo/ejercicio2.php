63d2732c075918c560ffc7fa294b0649210b912c
uriel <uriel.palalo@educa.jcyl.es>
2021-10-08 15:10:13 +0000

    Ejercicio 2 a medias

--- codigo/ejercicio2.php
+++ codigo/ejercicio2.php
@@ -1,8 +1,27 @@
+<h1>Ejercicio 2</h1>
 <?php
+//Para verlo introducir una variable enla url
+$recibida = $_GET["mivariable"];
+//Muestro la variable
+echo $recibida;
+echo "<p>";
+//Compruebo si es numerica
+if(is_numeric($recibida)==1){
+    echo "Es numérico";
+    
+    echo "<p>";
+    if(is_integer($recibida)==1){
+        echo "Es entero";
+        
+    }else{
+        echo "Es float";
+    }
+}
+
+
 
 echo "<br>";
 ?>
-<a href="">Enlace a la pagina a crear</a>
 <a href="codigo.php?paginaPHP=<?$pagina = basename($_SERVER['SCRIPT_FILENAME']);echo $pagina;?>">Ver el codigo</a>
 
 
